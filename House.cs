95dfe8cb187dbec66daa5d6f7410b6bb94d32046
Matthew Fernandez <mlf22198910@gmail.com>
2017-01-17 13:54:53 -0500

    Added four private fields

---
+++ House.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousesExample
+{
+    class House
+    {
+        private string color; //private prevents alterations
+        private int rooms;
+        private int size;
+        private int doors;
+    }
+}
